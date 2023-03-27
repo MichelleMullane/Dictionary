d1ef9e8c115a8febfa9b6347f1fdc0efcaf0489f
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2023-03-27 16:39:54 -0400

    added search function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header mt-5">Dictionary App</header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Michelle Mullane</small>
+      </footer>
     </div>
   );
 }
