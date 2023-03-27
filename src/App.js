1b26cff62a5c435944b2c8087256f062c6235b57
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2023-03-27 15:53:07 -0400

    installed bootstrap

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header mt-5">Dictionary App</header>
+    </div>
+  );
+}
+
+export default App;
